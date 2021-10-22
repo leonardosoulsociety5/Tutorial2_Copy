e6715a55a8a83334db43a8eadbd0b2706b629ceb
Leonardo Maranhao <89556741+leonardosoulsociety5@users.noreply.github.com>
2021-10-22 19:36:46 -0400

    first commit

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -16,6 +16,9 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+        if(target!=null)
+        {
+         this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+        }
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,19 +10,51 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     public Text score;
     private int scoreValue = 0;
-
+    public Text win;
+    public Text Lives;
+    private int LivesValue = 3;
+    public Text Lose;
+    private bool facingRight = true;
+    Animator anim;
+    private int level = 1;
+   
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Score: " + scoreValue.ToString();
+        win.text = "";
+        Lives.text = "Lives: " + LivesValue.ToString();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        anim.SetBool("isMoving", true);
+        if (scoreValue==4 && level == 1)
+        {
+            gameObject.transform.position = new Vector2(67f, 0.15f);
+            level++;
+            LivesValue = 3;
+            Lives.text = "Lives: " + LivesValue.ToString();
+        }
+        if(scoreValue == 8 && level == 2)
+        {
+            win.text = "You win! Game by Leonardo Maranhao";
+        }
         float hozMovement = Input.GetAxis("Horizontal");
         float verMovement = Input.GetAxis("Vertical");
+
+        if(rd2d.velocity == new Vector2(0.0f, 0.0f)){
+            anim.SetBool("isMoving", false);
+        }
+
+        if (LivesValue==0)
+        {
+            Destroy(gameObject);
+            Lose.text = "You lose,sorry!";
+        }
 
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
 
@@ -30,27 +62,57 @@ public class PlayerScript : MonoBehaviour
         {
             Application.Quit();
         }   
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+         {
+            Flip();
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.collider.tag == "Coin")
+        anim.SetBool("isGrounded", true);
+        anim.SetBool("isJumping", false);
+        if(collision.gameObject.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
-            Destroy(collision.collider.gameObject);
+            score.text = "Score: " + scoreValue.ToString();
+            Destroy(collision.gameObject);
+        }    
+        if(collision.gameObject.tag == "Enemy")
+        {
+            LivesValue -= 1;
+            Lives.text = "Lives: " + LivesValue.ToString();
+            Destroy(collision.gameObject);
+            
         }    
     }
     
     private void OnCollisionStay2D(Collision2D collision)
     {
+        anim.SetBool("isGrounded", true);
+        anim.SetBool("isJumping", false);
         if(collision.collider.tag == "Ground")
         {
             if(Input.GetKey(KeyCode.W))
             {
+                anim.SetBool("isGrounded", false);
+                anim.SetBool("isJumping", true);
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }    
         }
     }
+
+    void Flip()
+   {
+     facingRight = !facingRight;
+     Vector2 Scaler = transform.localScale;
+     Scaler.x = Scaler.x * -1;
+     transform.localScale = Scaler;
+   }
 
 }
